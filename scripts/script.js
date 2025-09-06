db5ac501c47265f453d72510b337b63f42b2bd8e
Md Rehan Ali <rehanali21d@gmail.com>
2025-09-06 14:00:20 +0600

    Added login functionality

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// Login button functionality
+
+document.getElementById('login').addEventListener('click', function(event){
+    event.preventDefault();
+    const mobileNumber = 123456789;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const pinNumberValue = document.getElementById('pin-number').value;
+
+    const mobileNumberValueInt = parseInt(mobileNumberValue);
+    const pinNumberValueInt = parseInt(pinNumberValue);
+
+    if(mobileNumber === mobileNumberValueInt && pinNumber === pinNumberValueInt){
+        window.location.href = "./home.html"
+    }
+    else{
+        alert("Invalid! For Mobile use: 123456789 & for pin use: 1234");
+    }
+})
